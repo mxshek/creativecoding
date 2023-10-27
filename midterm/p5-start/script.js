761894e1df031245d3eef686b4475c96d245b5cd
mxshek <ms9198@nyu.edu>
2023-10-26 23:17:42 -0400

    edited fontsize

--- midterm/p5-start/script.js
+++ midterm/p5-start/script.js
@@ -1,7 +1,18 @@
+//code start screen using timeout, give instructions
+//greet user, invite to center themselves, reveal instructions
+//'insert text'
+
+// setTimeout (
+//   function() {
+//     //fade from white to blue
+  
+//   }
+// )
 
 //create class for paintballs
 // click to launch paint
 let splatter = []
+let tinySplatter = []
 class Paintball{
   constructor(_x, _y, _r){
     this.x = _x
@@ -18,9 +29,21 @@ class Paintball{
 //   noStroke()}
 
 display() {
+  //main paintball
   fill(this.color)
   noStroke()
   ellipse(this.x, this.y, this.r)
+  
+
+  //splatters
+  //maybe I continue to translate origin?
+  // push()
+  // for (let i = 0; i < 5; i++) {
+
+  // }
+  // translate(this.x + this.r, this.y + this.r)
+  // ellipse(0, 0, this.r/n)
+  // pop()
 }
 }
 
@@ -46,7 +69,9 @@ class Wiper{
 function keyPressed() {
   if(keyCode === 32) {
     let w = new Wiper(random(0, 600), random(0,600), 20, 50);
-    eraser.push(w   )
+    eraser.push(w)
+    //try adding animation
+
   }
 }
 
@@ -63,7 +88,7 @@ function setup() {
 
 function draw() {
 //for testing purposes, set background color
-background(200 )
+background(255)
 
 
 //create for loop for paintball class to click to launch paint 
@@ -81,9 +106,11 @@ let d = 'Globs of mismatched paint.'
 let e = 'Brushstrokes in drunken stupor.' 
 let f = 'Resonating view.'
 fill(255)
-textSize(24);
-text(t, 190, 250);
-textSize(16);
+textSize(28);
+textStyle(BOLD)
+text(t, 190, 240);
+textSize(18);
+textStyle(NORMAL)
 text(d, 190, 300);
 text(e, 190, 325);
 text(f, 190, 350)
