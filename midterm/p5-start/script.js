9d50c73b3fdfa5161e6573bb8792ae50d50f0886
mxshek <ms9198@nyu.edu>
2023-10-29 15:28:58 -0400

    added intro sequence

--- midterm/p5-start/script.js
+++ midterm/p5-start/script.js
@@ -1,18 +1,11 @@
+
 //code start screen using timeout, give instructions
 //greet user, invite to center themselves, reveal instructions
 //'insert text'
 
-// setTimeout (
-//   function() {
-//     //fade from white to blue
-  
-//   }
-// )
-
 //create class for paintballs
 // click to launch paint
 let splatter = []
-let tinySplatter = []
 class Paintball{
   constructor(_x, _y, _r){
     this.x = _x
@@ -29,21 +22,9 @@ class Paintball{
 //   noStroke()}
 
 display() {
-  //main paintball
   fill(this.color)
   noStroke()
   ellipse(this.x, this.y, this.r)
-  
-
-  //splatters
-  //maybe I continue to translate origin?
-  // push()
-  // for (let i = 0; i < 5; i++) {
-
-  // }
-  // translate(this.x + this.r, this.y + this.r)
-  // ellipse(0, 0, this.r/n)
-  // pop()
 }
 }
 
@@ -69,9 +50,7 @@ class Wiper{
 function keyPressed() {
   if(keyCode === 32) {
     let w = new Wiper(random(0, 600), random(0,600), 20, 50);
-    eraser.push(w)
-    //try adding animation
-
+    eraser.push(w   )
   }
 }
 
@@ -86,10 +65,62 @@ function setup() {
 
 }
 
+let bg = [255, 255, 255]
+let hue = chroma.hsl(230, 1 , 0.5)
+
+
+//FOR WHEN MIYA PICKS UP AGAIN, HSL IS NOT IN CORRECT NOTATION USE REVIEW TIME TO SHIFT THIS
 function draw() {
 //for testing purposes, set background color
-background(255)
+background(bg)
 
+//INTRODUCTION
+//note for miya - 300 frames is about 10 sec
+if(frameCount > 100 && frameCount < 300) {
+  bg = [115, 215, 255]  
+  fill(255)
+  textSize(36)
+  textAlign(CENTER)
+  text('welcome', 300, 300)
+}
+
+if(frameCount > 300 && frameCount < 500) {
+  bg = [145, 224, 255]
+  fill(255)
+  textSize(36)
+  textAlign(CENTER)
+  text('take a breath', 300, 300)
+}
+
+if(frameCount > 500 && frameCount < 700) {
+  bg = [165, 229, 255]
+  fill(255)
+  textSize(36)
+  textAlign(CENTER)
+  text('exhale', 300, 300)
+}
+
+if(frameCount > 700 && frameCount < 1000) {
+  bg = [185, 235, 255]
+  fill(255)
+  textSize(36)
+  textAlign(CENTER)
+  text('you are exactly', 300, 275)
+  text('as you are meant to be', 300, 325)
+
+}
+
+if(frameCount > 1000 && frameCount < 1300) {
+  bg = [225, 246, 255]
+  fill(200)
+  textSize(36)
+  textAlign(CENTER)
+  text('CLICK to create', 300, 275)
+  text('press SPACE to erase', 300, 325)
+}
+
+if(frameCount > 1400) {
+  bg = [255, 255, 255]
 
 //create for loop for paintball class to click to launch paint 
 for(let i = 0; i < splatter.length; i++) {
@@ -106,6 +137,7 @@ let d = 'Globs of mismatched paint.'
 let e = 'Brushstrokes in drunken stupor.' 
 let f = 'Resonating view.'
 fill(255)
+textAlign(LEFT)
 textSize(28);
 textStyle(BOLD)
 text(t, 190, 240);
@@ -114,6 +146,12 @@ textStyle(NORMAL)
 text(d, 190, 300);
 text(e, 190, 325);
 text(f, 190, 350)
+}
+
+
+
+
+
 
 }
 
