dc90ea088d8d105fbf42fa03133be01df083626f
mxshek <ms9198@nyu.edu>
2023-12-11 11:53:17 -0500

    included weather API and testing question class

--- final project/class_11_27/p5-start/script.js
+++ final project/class_11_27/p5-start/script.js
@@ -1,11 +1,40 @@
+let bg = [0, 0, 0]
+let currentTemp
 
+conversation = []
 
-function setup() {
-  createCanvas(600, 600)
+//class for questions?
+class Question{
+  constructor(name, _x, _y, ) {
+    this.name = name;
+    this.x = _x;
+    this.y = _y;
+  }
+
+  display() {
+    text(this.name, this.x, this.y)
+    textAlign(CENTER, CENTER)
+    textSize(32)
+    
+  }
 
 }
 
+setTimeout(
+  function() {
+    console.log("does this work")
+    bg = [100, 100, 100]
+    //create new text class and then push array? 
+  }, 2000
+)
+
+function setup() {
+  createCanvas(600, 600)
+  apiRequest()
+}
+
 function draw() {
+background(bg)
 
   //yes box
   rectMode(CENTER)
@@ -21,18 +50,47 @@ function draw() {
   textSize(32)
   text('no', 350, 350)
 
-console.log(frameCount)
+  // console.log(frameCount)
 
   //draw intro sequence and add arduino in later
-if (frameCount < 1000) {
-  text('hello', 300, 200)
-  textAlign(CENTER)
-}
+// if (frameCount < 1000) {
+//   text('hello', 300, 200)
+//   textAlign(CENTER)
+// }
 
-if(frameCount > 1000 && frameCount < 2000){
-  text('will you answer my question', 300, 200)
-  textAlign(CENTER)
-}
+// if(frameCount > 1000 && frameCount < 2000){
+//   text('will you answer my question', 300, 200)
+//   textAlign(CENTER)
+// }
+
+  //input API request
+  if (currentTemp != undefined) {
+    for(let i = 0; i < currentTemp.length; i++){
+      let h = map(currentTemp[i], 0, 100, height, 0)
+      rect(i*width/currentTemp.length, h, 100, 25)
+      fill(80, 200, 100)
+    }
+  }
+
+  }
+
+  async function apiRequest() {
+    let request = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,is_day,weather_code&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1")
+    console.log(request)
+    
+    let data = await request.json()
+    console.log(data)
+
+    let nowTemp = data.current
+    console.log("the current values are:")
+    console.log(nowTemp)
+
+    currentTemp = nowTemp.temperature_2m
+    console.log("the current temperature is ")
+    console.log(currentTemp)
+
+    
+
   }
 
   // How are you feeling?
