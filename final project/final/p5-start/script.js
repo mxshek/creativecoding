62bd7d0aa769288243f95e41cda2c6431711b945
mxshek <ms9198@nyu.edu>
2023-12-15 12:23:46 -0500

    uploaded arduino

--- final project/final/p5-start/script.js
+++ final project/final/p5-start/script.js
@@ -1,136 +1,85 @@
-let bg = [0, 0, 0]
-let currentTemp
-let x
+// add the same uuid as your arduino
+const serviceUuid = "5af87508-4bf9-4852-8faf-91252c8afef0";
 
-let questionArr = []
+let buttonCharacteristic;
+let buttontwoCharacteristic;
+let photoCharacteristic;
 
-//class for questions?
-class Question{
-  constructor(name, _x, _y, ) {
-    this.name = name;
-    this.x = _x;
-    this.y = _y;
-  }
+let buttonValue = 0;
+let buttontwoValue = 0;
+let photoValue = 0;
 
-  display() {
-    text(this.name, this.x, this.y)
-    textAlign(CENTER)
-    textSize(32)
-
-  }
-
-}
-
-
-setTimeout(
-  function() {
-    console.log("does this work")
-    bg = [100, 100, 100]
-    //create new text class and then push array? 
-    let x = "test"
-    questionArr.push()
-  }, 2000
-)
+let myBLE;
 
 function setup() {
-  createCanvas(600, 600)
-  apiRequest()
+  createCanvas(windowWidth, windowHeight - 100)
+  //object will let us read to the bluetooth and write to it as well
+  myBLE = new p5ble();
 
-  for (let i = 0; i < questionArr.length; i++) {
-    questionArr[i] = new Question(x, 300, 200)
-  }
+  //turn on bluetooth connection
+  const connectButton = createButton("Connect");
+  connectButton.mousePressed(connectToBLE);
 }
 
+//ESTABLISH BUTTON is YES
+//BUTTONTWO is NO
+
 function draw() {
-background(bg)
-
-  //yes box
-  rectMode(CENTER)
-  rect(200, 400, 100, 50)
-  fill(0, 255, 0)
-  textSize(32)
-  text('yes', 150, 350)
-
-  //no box
-  rectMode(CENTER)
-  rect(400, 400, 100, 50)
-  fill(255, 0 , 255)
-  textSize(32)
-  text('no', 350, 350)
-
-  //trying to add a new object
-  for (let i = 0; i < questionArr.length; i++){
-    questionArr[i].display
-  }
-
-  // console.log(frameCount)
-
-  //draw intro sequence and add arduino in later
-// if (frameCount < 1000) {
-//   text('hello', 300, 200)
-//   textAlign(CENTER)
-// }
-
-// if(frameCount > 1000 && frameCount < 2000){
-//   text('will you answer my question', 300, 200)
-//   textAlign(CENTER)
-// }
-
-  //input API request
-  if (currentTemp != undefined) {
-    for(let i = 0; i < currentTemp.length; i++){
-      let h = map(currentTemp[i], 0, 100, height, 0)
-      rect(i*width/currentTemp.length, h, 100, 25)
-      fill(80, 200, 100)
-    }
-  }
-
-  }
-
-  async function apiRequest() {
-    let request = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,is_day,weather_code&hourly=temperature_2m&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&forecast_days=1")
-    console.log(request)
-    
-    let data = await request.json()
-    console.log(data)
-
-    let nowTemp = data.current
-    console.log("the current values are:")
-    console.log(nowTemp)
-
-    currentTemp = nowTemp.temperature_2m
-    console.log("the current temperature is ")
-    console.log(currentTemp)
-
-    
-
-  }
-
-  // How are you feeling?
-  // What does it mean to be human?
-  // Do you feel connected?
-  // Does this weather make you happy? [Weather API?]
-  // This is the weather in [pick from API JSON]
-  // Are you in this place?
-  // Do you want to be?
-  // Are you tired of my questions? 
-  // Are you tired of me? 
-  // What does it mean to be human?
-  // Am I human?	
-  // [Create custom quote JSON]
-  // I found this. Does it make you happy?
-  // Have you enjoyed our conversation?
-  // I wanted to make this conversation enjoyable. I like your perspective. 
-  // Thank you for spending time with me. 
-  // Have you said that to anyone lately?
-  // Thank you for being human.
-  // Have you said that to anyone lately?
-
-  // function mouseClicked(){
-  //   if(frameCount > 1000 && frameCount < 2000 && mouseX > 150 && mouseX < 250 && mouseY > 375 && mouseY < 425){
-  //     //new class? should i make questions a class and then push them? 
-
-
-  // }
+  background(0)
   
-  // }
+    if (buttonValue > 0) {
+    background(126)
+  } else { 
+    background(0)
+  }
+
+
+  // fill(255, 30, 45);
+  // ellipse(width / 2, height / 2, 100)
+  fill(photoValue, 30, 45);
+  ellipse(width / 2, height / 2, 300)
+}
+
+function connectToBLE() {
+  myBLE.connect(serviceUuid, gotCharacteristics)
+}
+
+function gotCharacteristics(error, characteristics) {
+  if (error) console.error(error)
+  // console.log("characteristics: ", characteristics)
+  console.log("Connected to Service")
+  
+  console.log("Characteristics: ", characteristics)
+
+  //check console of sketch and then double check arduino which uuid is which
+  buttonCharacteristic = characteristics[0]
+  myBLE.read(buttonCharacteristic, gotButtonValue)
+
+  buttontwoCharacteristic = characteristics[1]
+  myBLE.read(buttontwoCharacteristic, gotbuttontwoValue)
+
+  photoCharacteristic = characteristics[2]
+  myBLE.read(photoCharacteristic, gotPhotoValue)
+
+}
+
+function gotButtonValue(error, value) {
+  if (error) console.error(error)
+  buttonValue = value
+// console.log("button value: ", value)
+  myBLE.read(buttonCharacteristic, gotButtonValue)
+}
+
+function gotbuttontwoValue(error, value) {
+  if (error) console.error(error)
+  buttontwoValue = value
+// console.log("button value: ", value)
+  myBLE.read(buttontwoCharacteristic, gotbuttontwoValue)
+}
+
+function gotPhotoValue(error, value) {
+  if (error) console.error(error)
+  photoValue = value
+// console.log("photocell value: ", value)
+  myBLE.read(photoCharacteristic, gotPhotoValue)
+}
