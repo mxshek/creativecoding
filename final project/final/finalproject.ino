d77e8e0b6abce8289e3f34057283292084a476ac
mxshek <ms9198@nyu.edu>
2023-12-18 09:52:07 -0500

    transfer arduino files

--- final project/final/finalproject.ino
+++ final project/final/finalproject.ino
@@ -1,62 +1,71 @@
+//open library manager
+//download it
 #include <ArduinoBLE.h>
 
 // get a uuid from https://www.uuidgenerator.net/
-const auto myUuid = "3fdbea33-5c5c-4b78-b782-1e4e5f0826a8";
-const auto buttonID = "33559562-1440-4e0d-8199-c1f076b6a830";
-const auto potID = "427bcdea-4cf5-4e45-bbf4-a3886a093a45";
-const auto photoID = "43ab08db-3e4e-46e7-b98e-7e8204853fcd";
+// version 4 uuid (first one on page) is good
+// new UUID for each thingy
+const auto myUuid = "5af87508-4bf9-4852-8faf-91252c8afef0";
+const auto buttonID = "c020bced-2edf-4363-8fa3-4ae1a1fa64d7";
+const auto buttontwoID = "d595f62d-4fe0-42bb-b543-b51e22237c4e";
+const auto photoID = "dabaf295-b66d-4877-a76c-6b8ef984b906";
 
 // pins for the led and sensors
 const int ledPin = 10;
 const int buttonPin = 2;
-const int potPin = A0;
+const int buttontwoPin = 3;
 const int photoPin = A1;
 
-// make a service with your uuid
+// make a service with your uuid 
+// make a variable called sensor service (name is sensor Service) and we are passing main UUID into this
+// BLE = Bluetooth Low Energy
 BLEService sensorService(myUuid);
 
 // sensor characteristics are read-only
 BLEIntCharacteristic buttonCharacteristic(buttonID, BLERead | BLENotify);
-BLEIntCharacteristic potCharacteristic(potID, BLERead | BLENotify);
+BLEIntCharacteristic buttontwoCharacteristic(buttontwoID, BLERead | BLENotify);
 BLEIntCharacteristic photoCharacteristic(photoID, BLERead | BLENotify);
-
-// led characteristic can be read and written to
-BLEByteCharacteristic ledCharacteristic(myUuid, BLERead | BLEWrite);
 
 void setup() {
   // Initiate Serial and wait for the port to open
   // not needed for communication, but helpful for debugging
   Serial.begin(9600);
-  while (!Serial); 
+  // pausing the code until the serial port opens, so comment this out for bluetooth
+  // while (!Serial); 
 
   // assign the digital pins
-  pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-  pinMode(potPin, INPUT);
+  pinMode(buttontwoPin, INPUT);
+  // pinMode(photoPin, INPUT);
 
   // throw an error if Bluetooth doesn't work
+  // if our bluetooth low energy device attached to arduino doesn't work, give us a message
   if (!BLE.begin()) {
     Serial.println("starting BLE module failed...");
     while (1);
   }
 
   // set the name of the peripheral
-  BLE.setLocalName("My Arduino");
+  // BLE.setLocalName("My Arduino");
   // set uuid of the peripheral
+  // BLE.setAdvertisedService(sensorService);
+  
+  //in class example of setting the name of the peripheral
+  BLE.setLocalName("Miya's Arduino");
   BLE.setAdvertisedService(sensorService);
 
   // add characteristics
   sensorService.addCharacteristic(buttonCharacteristic);
-  sensorService.addCharacteristic(potCharacteristic);
+  sensorService.addCharacteristic(buttontwoCharacteristic);
   sensorService.addCharacteristic(photoCharacteristic);
 
   // add the service
   BLE.addService(sensorService);
 
   // set initial values
-  ledCharacteristic.writeValue(0);
+  // ledCharacteristic.writeValue(0);  
   buttonCharacteristic.writeValue(0);
-  potCharacteristic.writeValue(0);
+  buttontwoCharacteristic.writeValue(0);
   photoCharacteristic.writeValue(0);
 
   // start advertising
@@ -71,39 +80,31 @@ void loop() {
 
   // read sensor states
   auto buttonValue = digitalRead(buttonPin);
-  auto potValue = analogRead(potPin) / 4;
+  auto buttontwoValue = digitalRead(buttontwoPin);
+  //divide by 4 because of bytes that bluetooth can get
   auto photoValue = analogRead(photoPin) / 4;
 
   // check if states have changed
   bool buttonChanged = (buttonCharacteristic.value() != buttonValue);
-  bool potChanged = (potCharacteristic.value() != potValue);
+  bool buttontwoChanged = (buttontwoCharacteristic.value() != buttontwoValue);
   bool photoChanged = (photoCharacteristic.value() != photoValue);
 
   // update the BLE characteristic if the state has changed
   if (buttonChanged) {
     buttonCharacteristic.writeValue(buttonValue);
+    // Serial.println("button: ");
     // Serial.println(buttonValue);
   }
-  if (potChanged) {
-    potCharacteristic.writeValue(potValue);
-    // Serial.println(potValue);
+  if (buttontwoChanged) {
+    buttontwoCharacteristic.writeValue(buttontwoValue);
+    // Serial.println("button two: ");
+    // Serial.println(buttontwoValue);
   } 
-  // if (photoChanged) {
+  if (photoChanged) {
     photoCharacteristic.writeValue(photoValue);
-    Serial.println(photoValue);
-  // }
-
-  // respond to and events which write to the LED characteristic
-  if (ledCharacteristic.written()) {
-    // true if high, false if low
-    if (ledCharacteristic.value()) {
-      Serial.println("LED on");
-      digitalWrite(ledPin, HIGH);
-    } else {
-      Serial.println("LED off");
-      digitalWrite(ledPin, LOW);
-    }
+    // Serial.println("photocell: ");
+    // Serial.println(photoValue);
   }
 
-  delay(100);
+  delay(10);
 }
